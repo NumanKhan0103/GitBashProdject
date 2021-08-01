34e7d0f0ad6c0577088f5027d7eeb3fcc3907ab3
Numan Dawar <numankhann405@gmail.com>
2021-08-01 00:58:10 -0700

    new file here

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 window.alert("HI Git hub form javascript");
+
+
+function new_user() {
+    alert("hi new user ");
+    if (true) {
+        // code 
+    } else {
+        //code
+        if (true) {
+            //code 
+        } else {
+
+        }
+    }
+}
