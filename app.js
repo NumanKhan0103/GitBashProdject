f741ac3baceb7c05938228afbdaf74334eb432a7
Numan Dawar <numankhann405@gmail.com>
2021-08-01 03:45:57 -0700

    style.css is add in index.html

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-window.alert("HI Git hub form javascript");
-
-
 function new_user() {
-    if(user_name){
+    if (user_name) {
         alert("new user is add");
     } else {
         alert("Not Add new User");
